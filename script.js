8d3c9b975a61096f1cc6409fcb218c761bb373d6
GabrielCSilva <211619696+GabrielCorreaDaSilva@users.noreply.github.com>
2025-07-17 03:40:22 -0300

    Add a function to play one round

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const computerChoice = getComputerChoice();
-const humanChoice = getHumanChoice();
-
-console.log(`Computer choice: ${computerChoice}`);
-console.log(`Human choice: ${humanChoice}`);
-
-
 function getComputerChoice() {
     switch(Math.floor(Math.random() * 3) + 1) {
         case 1:
@@ -20,3 +13,38 @@ function getHumanChoice() {
     const anwser = prompt('What do chose: Rock, Paper or Scissors? ')
     return anwser.toLowerCase();
 }
+
+function playRound(computerChoice, humanChoice)
+ {
+    console.log(`Computer choice: ${computerSelection} | Human choice: ${humanSelection}`);
+
+    if(computerChoice === humanChoice) {
+        console.log(`Draw! ${computerChoice} vs ${humanChoice}`);
+    } else if(humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log(`Player win! ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
+    }else if(humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log(`Player win! ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
+    } else if(humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log(`Player win! ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
+    } else {
+        console.log(`Computer win! ${computerChoice} beats ${humanChoice}`);
+        computerScore += 1;
+    }
+    
+    console.log(`Computer score: ${computerScore} | Human score: ${humanScore} `);
+}
+
+
+
+let computerScore = 0;
+let humanScore = 0;
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+
+
+playRound(computerSelection, humanSelection);
+
